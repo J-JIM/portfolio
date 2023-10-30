8d9e808aae310de401ba465b5504743d68df06ce
J-JIM <86223188+J-JIM@users.noreply.github.com>
2023-10-31 08:53:22 +0900

    bronze1 2693.cpp

---
+++ 2693.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void){
+    int t;
+    cin >> t;
+
+    for(int i = 0; i < t; i++){
+        int A[10];
+        int temp = 1001;
+        for(int j = 0; j < 10 ; j++){
+            cin >> A[j];         
+        }
+
+    }
+}
